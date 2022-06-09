56493c7b3da079e03b9c34654b252cd250a3b56d
unknown <you@example.com>
2022-06-09 22:17:46 +0600

    add to navbar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
+
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Home from './Portfolio/Home';
+import About from './Portfolio/About';
+import Contact from './Portfolio/Contact';
+import Services from './Portfolio/Services';
+import Projects from './Portfolio/Projects';
+import Resume from './Portfolio/Resume';
+import Footer from './Portfolio/Shared/Footer';
+import Navbar from './Portfolio/Shared/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar></Navbar>
+      <Routes>
+        <Route path="/" element={<Home />} ></Route>
+        <Route path="/home" element={<Home />} ></Route>
+        <Route path="/about" element={<About />} ></Route>
+        <Route path="/contact" element={<Contact />} ></Route>
+        <Route path="/services" element={<Services />} ></Route>
+        <Route path="/projects" element={<Projects />} ></Route>
+        <Route path="/resume" element={<Resume />} ></Route>
+      </Routes>
+      <Footer></Footer>
     </div>
   );
 }
---
+++ src/Portfolio/Home.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div className='home-container'>
+            <div className='home-parent'>
+            <div className='home-details'>
+            <div className='cols'>
+                        <a href="#">
+                            
+             </a>
+         </div>
+        </div>
+     </div>
+    </div>
+    );
+};
+
+export default Home;
